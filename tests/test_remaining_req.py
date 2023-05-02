8e10edcb7cf2c1785ce8d132bb87fd0b14414cbf
jihye <ghe.lee19@gmail.com>
2023-05-02 17:26:11 +0900

    test: TestRemainingReq 클래스 작성

--- tests/test_remaining_req.py
+++ tests/test_remaining_req.py
@@ -2,36 +2,34 @@ import datetime
 
 import pytest
 
-import upbit as _upbit
-from upbit import RemainingReq, InvalidRemainingReq
+from upbit import RemainingReq, InvalidRemainingReq, Upbit
 
 
-def test_remaining_req_class():
-    remaining_req = 'group=candles; min=60; sec=10'
-    instance = RemainingReq(remaining_req)
-    assert instance.group == 'candles'
-    assert instance.minute == 60
-    assert instance.second == 10
-    assert isinstance(instance.updated, datetime.datetime)
+class TestRemainingReq:
+    def test_property(self):
+        remaining_req = 'group=candles; min=60; sec=10'
+        instance = RemainingReq(remaining_req)
+        assert instance.group == 'candles'
+        assert instance.minute == 60
+        assert instance.second == 10
+        assert isinstance(instance.updated, datetime.datetime)
 
+    @pytest.mark.parametrize(
+        "exception, remaining_req",
+        (
+            (InvalidRemainingReq, ""),
+            (InvalidRemainingReq, "group=candles; min=60"),
+            (InvalidRemainingReq, "group=candles; min=60; sec="),
+        ),
+    )
+    def test_invalid_remaining_req(self, exception, remaining_req):
+        with pytest.raises(exception):
+            RemainingReq(remaining_req)
 
-@pytest.mark.parametrize(
-    "exception, remaining_req",
-    (
-        (InvalidRemainingReq, ""),
-        (InvalidRemainingReq, "group=candles; min=60"),
-        (InvalidRemainingReq, "group=candles; min=60; sec="),
-    ),
-)
-def test_invalid_remaining_req(exception, remaining_req):
-    with pytest.raises(exception):
-        RemainingReq(remaining_req)
+    def test_remaining_req_cache(self):
+        up = Upbit()
+        assert up.get_remaining_reqs('market') is None
 
-
-def test_remaining_req_cache():
-    upbit = _upbit.Upbit()
-    assert upbit.get_remaining_reqs('market') is None
-
-    # todo mock
-    upbit.get_markets()
-    assert isinstance(upbit.get_remaining_reqs('market'), RemainingReq)
+        # todo mock
+        up.get_markets()
+        assert isinstance(up.get_remaining_reqs('market'), RemainingReq)
